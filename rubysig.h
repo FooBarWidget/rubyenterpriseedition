9ef6c47b29c2ac5bb5d742b7a69d07fa4e95950e
Brent Roman <brent@mbari.org>
2009-02-27 10:32:16 -0800

    #defined __anyPowerPC__

--- rubysig.h
+++ rubysig.h
@@ -15,6 +15,11 @@
 
 #include <errno.h>
 
+#if defined __ppc__ || defined __powerpc__ || \
+    defined __ppc64__ || defined __powerpc64__
+#define __anyPowerPC__ 1  /* for compatibility with older gcc versions */
+#endif
+
 /* STACK_WIPE_SITES determines where attempts are made to exorcise
    "ghost object refereces" from the stack and how the stack is cleared:
    
@@ -65,7 +70,7 @@
 #ifndef STACK_WIPE_SITES
 # ifdef __x86_64__     /* deal with "red zone" by not inlining stack clearing */
 #  define STACK_WIPE_SITES  0x6770
-# elif defined __ppc__ || defined __ppc64__   /* On any PowerPC, deal with... */
+# elif defined __anyPowerPC__   /* On any PowerPC, deal with... */
 #  define STACK_WIPE_SITES  0x7764   /* red zone & alloc(0) doesn't return sp */
 # else
 #  define STACK_WIPE_SITES  0x8770 /*normal case, use 0x4770 if problems arise*/
@@ -211,7 +216,7 @@ static inline VALUE *__sp(void) \
   VALUE *sp; asm(asmb); \
   return sp; \
 }
-#  if defined __ppc__ || defined __ppc64__
+#  ifdef __anyPowerPC__
 __defspfn("addi %0, r1, 0": "=r"(sp))
 #  elif defined  __i386__
 __defspfn("movl %%esp, %0": "=r"(sp))
