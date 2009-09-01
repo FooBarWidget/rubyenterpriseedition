11557d534ea678fb424ec8009c41221cb0713d7f
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-09-01 13:58:37 +0200

    Upgrade to RubyGems 1.3.5.

--- distro/tasks.rb
+++ distro/tasks.rb
@@ -9,7 +9,7 @@ REE_VERSION = begin
 	$1
 end
 DISTDIR = "ruby-enterprise-#{VENDOR_RUBY_VERSION}-#{REE_VERSION}"
-RUBYGEMS_URL = "http://rubyforge.org/frs/download.php/55066/rubygems-1.3.2.tgz"
+RUBYGEMS_URL = "http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz"
 RUBYGEMS_PACKAGE = RUBYGEMS_URL.sub(/.*\//, '')
 
 desc "Create a distribution directory"
